aa6cb5de7894c08180ca92aa4bc1b003d49eb105
believe-gautam <81441165+believe-gautam@users.noreply.github.com>
2022-07-21 12:35:34 +0530

    footer-update

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/common/Header/';
-import Footer from "../components/common/Footer/Index";
+import Footer from "../components/common/Footer/";
 const Layout = (props) => {
     return (< >
 
